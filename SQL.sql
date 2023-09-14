e9fce358064d66b1779b11f3117a96beeff2bc71
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-09-14 15:35:11 +0900

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,18 @@
+SELECT * FROM CUSTOMERS WHERE country = 'USA';
+
+CREATE	Creates a new table, a view of a table, or other object in the database.	Demo
+ALTER	Modifies an existing database object, such as a table.	Demo
+DROP	Deletes an entire table, a view of a table or other objects in the database.	Demo
+TRUNCATE	Truncates the entire table in a go.	Demo
+
+
+SELECT	Retrieves certain records from one or more tables.	Demo
+INSERT	Creates a record.	Demo
+UPDATE	Modifies records.	Demo
+DELETE	Deletes records.	Demo
+
+
+GRANT	Gives a privilege to user	Demo
+REVOKE	Takes back privileges granted from user.	Demo
+
+
